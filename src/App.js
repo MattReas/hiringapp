566a33721f5c4588e27cf53a9490fe6e853d546d
MattReas <112966935+MattReas@users.noreply.github.com>
2023-06-25 21:25:38 -0500

    Implemented Routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-
+// imports that are not components
 import React, { useState } from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+//css
+import "./style/App.css";
+// Components
 import ApplicantGrid from "./components/ApplicantGrid";
-import './style/App.css';
 import FilterDropdown from "./components/FilterDropDown";
 import NavigationBar from "./components/Navbar";
 import ProfileCreationform from "./components/ProfileCreation";
@@ -10,27 +13,44 @@ import ProfileCreationform from "./components/ProfileCreation";
 function App() {
   const applicants = [
     {
-    name: "Matt Reas", ePanther: "mreas", major: "Computer Science", grad: "Spring 2024", position: "HD1"
-  }, {
-    name: "Ben Reas", ePanther: "reas", major: "Computer Science", grad: "Fall 2025", position: "HD2"
-  }
-  ]
+      name: "Matt Reas",
+      ePanther: "mreas",
+      major: "Computer Science",
+      grad: "Spring 2024",
+      position: "HD1",
+    },
+    {
+      name: "Ben Reas",
+      ePanther: "reas",
+      major: "Computer Science",
+      grad: "Fall 2025",
+      position: "HD2",
+    },
+  ];
 
   const [filterPosition, setFilterPosition] = useState("");
 
-  const filteredApplicants = filterPosition ? applicants.filter(applicant => applicant.position === filterPosition) : applicants
- 
+  const filteredApplicants = filterPosition
+    ? applicants.filter((applicant) => applicant.position === filterPosition)
+    : applicants;
+
   const handleFilterChange = (newPosition) => {
     setFilterPosition(newPosition);
-  }
+  };
 
   return (
-    <div className="App">
-      <NavigationBar/>
-      <FilterDropdown onChange={handleFilterChange}/>
-      <ApplicantGrid applicants={filteredApplicants}/>
-      <ProfileCreationform/>
-    </div>
+    <Router>
+      <div className="App">
+        <NavigationBar />
+        <Routes>
+          <Route path="/" element={<>
+            <FilterDropdown onChange={handleFilterChange}/>
+            <ApplicantGrid applicants={filteredApplicants}/>
+          </>} />
+          <Route path="create-profile" element={<ProfileCreationform />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,24 @@
-import React from 'react';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import React from "react";
+import { Link } from "react-router-dom"
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+
 
 function NavigationBar() {
-    return(
+  return (
     <Navbar expand="lg" className="bg-body-tertiary">
-        <Container>
-            <Navbar.Brand href="#home">Hiring Manager</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className='me-auto'>
-                <Nav.Link href='#home'>Home</Nav.Link>
-                <Nav.Link href='#ProfileCreation'>Profile Creation</Nav.Link>
-            </Nav>
-            </Navbar.Collapse>
-        </Container>
+      <Container>
+        <Navbar.Brand as={Link} to="/">Hiring Manager</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to="/">Home</Nav.Link>
+            <Nav.Link as={Link} to="/create-profile">Profile Creation</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
     </Navbar>
-    )
+  );
 }
-export default NavigationBar
+export default NavigationBar;
